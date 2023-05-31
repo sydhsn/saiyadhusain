6f71abab2bf9ba1b9b76aca7f457af038563572e
Mohammad Saiyad Husain <36475318+sydhsn@users.noreply.github.com>
2023-06-01 02:20:30 +0530

    Update about.js
    Text spelling correction

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,11 +13,11 @@ const About = () => {
             <p className="h6 text-muted" id="about">About Me,</p>
             <p className="h3">Frontend & UI Developer. <br /> Mumbai-India.</p>
             <p className="h6 text-muted">
-              I am Saiyad Husain a FrontEnd / UI Developer based in Mumbai,India. I have over 6.5 years of experience in Web and Mobile Application.
-              Hard work, Challenging & passionate about developing, performance, scalable code and thoughtful design for applications.Technical and highly motivated front end developer, with technical skills in end-to-end software development, web and mobile application designing,
-possessing knowledge of various software languages and tools with problems solvong and passion about the user experience and integration of software as per the requirments.
 
-                </p>
+I am Saiyad Husain, a front-end and UI developer based in Mumbai,India. I have over 8 years of experience in web and mobile applications.
+Hard work, challenging and passionate about developing, performance, scalable code, and thoughtful design for applications Technical and highly motivated front-end developers with technical skills in end-to-end software development, web and mobile application design,
+possessing knowledge of various software languages and tools with problem solving and passion about the user experience and integration of software as per the requirements 
+</p>
             <a className="btn btn-sm-12 mt-5 btn-lg theme-btn" href={resume}><i className="fa fa-download fa-right text-a" />Download Profile</a>
           </div>
         </div>
