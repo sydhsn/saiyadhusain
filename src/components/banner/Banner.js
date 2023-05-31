24388627c5a5c37c562af69a6ffe776f08aa2e06
Mohammad Saiyad Husain <36475318+sydhsn@users.noreply.github.com>
2023-06-01 02:13:41 +0530

    Update Banner.js
    Senior

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
           <div className="col-sm-6 mob-center align-self-center justify-content-left">
                 <p className="h6 text-muted">HEY THERE !</p>
                 <p className="h3">&ldquo;I AM SAIYAD HUSAIN&rdquo;</p>
-                <p className="h6 text-muted"><cite>Sr. Software Engineer</cite></p>
+                <p className="h6 text-muted"><cite>Senior Software Engineer</cite></p>
                 <a className="btn btn-sm-12 mt-5 btn-lg theme-btn" href="tel:+918652431765"><i className="fa fa-mobile fa-right text-a"/>Call me</a>
             </div>
             <div className="col-sm-6 m-0 p-0">
